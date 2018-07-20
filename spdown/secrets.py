007a876b381a8ef42bbece284c2e1fda2b401b47
beremaran <beremaran@gmail.com>
2018-07-21 01:10:21 +0300

    convert Secrets class to non-static

--- spdown/secrets.py
+++ spdown/secrets.py
@@ -29,44 +29,42 @@ import json
 
 
 class Secrets:
-    _secret_file: str = None
-    _secrets: dict = None
+    _secret_file: str
+    _secrets: dict
 
-    @staticmethod
-    def set_secret_file(path):
-        Secrets._secret_file = path
+    def __init__(self, secret_file: str = None, secrets: dict = None):
+        self._secret_file = secret_file
+        self._secrets = secrets
 
-    @staticmethod
-    def _find_secret_file():
+    def set_secret_file(self, path):
+        self._secret_file = path
+
+    def _find_secret_file(self):
         if os.path.exists('secrets.json'):
-            Secrets._secret_file = 'secrets.json'
+            self._secret_file = 'secrets.json'
             return
 
-        Secrets._secret_file = None
+        self._secret_file = None
 
-    @staticmethod
-    def _load():
-        if Secrets._secret_file is None:
-            Secrets._find_secret_file()
-        if Secrets._secrets is None:
-            with open(Secrets._secret_file, 'r') as f:
-                Secrets._secrets = json.load(f)
+    def _load(self):
+        if self._secret_file is None:
+            self._find_secret_file()
+        if self._secrets is None:
+            with open(self._secret_file, 'r') as f:
+                self._secrets = json.load(f)
 
-    @staticmethod
-    def get_spotify_credentials() -> tuple:
-        Secrets._load()
-        spotify_secrets = Secrets._secrets['spotify']
+    def get_spotify_credentials(self) -> tuple:
+        self._load()
+        spotify_secrets = self._secrets['spotify']
         return spotify_secrets['client_id'], spotify_secrets['client_secret']
 
-    @staticmethod
-    def get_spotify_username() -> str:
-        Secrets._load()
-        if 'username' in Secrets._secrets['spotify'].keys():
-            return Secrets._secrets['spotify']['username']
+    def get_spotify_username(self) -> str:
+        self._load()
+        if 'username' in self._secrets['spotify'].keys():
+            return self._secrets['spotify']['username']
         else:
             return ''
 
-    @staticmethod
-    def get_youtube_dev_key() -> str:
-        Secrets._load()
-        return Secrets._secrets['youtube']['developer_key']
+    def get_youtube_dev_key(self) -> str:
+        self._load()
+        return self._secrets['youtube']['developer_key']
