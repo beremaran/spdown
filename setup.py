e4e44f5e32eddd16d00d5921d6572b57226bf696
beremaran <beremaran@gmail.com>
2018-07-21 03:04:02 +0300

    increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='spdown',
-    version='0.0.4',
+    version='0.0.5',
     author='Berke Emrecan Arslan',
     author_email='berke@beremaran.com',
     description='Download Spotify playlists from YouTube',
