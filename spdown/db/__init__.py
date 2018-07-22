c19a3183968668ba1e328651e018158003975162
beremaran <beremaran@gmail.com>
2018-07-23 02:40:46 +0300

    initialize database session and schema

---
+++ spdown/db/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from spdown.db.models import *
+from spdown.config import Config
+
+config = Config()
+db_path = config.get('database_path')
+del config
+del Config
+
+engine = create_engine('sqlite:///{}'.format(db_path), echo=True)
+Base.metadata.create_all(engine)
+session = sessionmaker()
+session.configure(bind=engine)
+session = session()
