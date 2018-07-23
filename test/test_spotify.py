ec97bf0b7f39d4afd5867c31e33d8f846722260b
beremaran <beremaran@gmail.com>
2018-07-23 05:06:09 +0300

    remove Spotify tests

--- test/test_spotify.py
+++ test/test_spotify.py
@@ -5,25 +5,8 @@ import unittest
 from spdown.spotify import Spotify
 
 
-
 class TestSpotifyExtraction(unittest.TestCase):
-    def test_track_count(self):
-        spotify = Spotify()
-        tracks, playlist_name = spotify.extract_tracks('spotify:user:beremaran:playlist:5NB6rw6Up1jL27x3I9jEmS')
-        self.assertEqual(len(tracks), 11)
-
-    def test_extract_track(self):
-        spotify = Spotify()
-
-        tracks, playlist_name = spotify.extract_tracks(
-            'spotify:user:beremaran:playlist:5NB6rw6Up1jL27x3I9jEmS'
-        )
-
-        for track in tracks:
-            self.assertIsNotNone(track.artist)
-            self.assertIsNotNone(track.title)
-            self.assertIsNotNone(track.spotify_id)
-            self.assertIsNotNone(track.album_name)
+    pass
 
 
 if __name__ == "__main__":
