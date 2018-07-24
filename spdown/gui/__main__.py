0704656be4d134033f50bc50324ecec7529e6140
beremaran <beremaran@gmail.com>
2018-07-24 20:10:30 +0300

    update configuration for gui before startup

--- spdown/gui/__main__.py
+++ spdown/gui/__main__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+from kivy import Config
+
 from spdown.gui.app import SPDown
 
 if __name__ == "__main__":
+    Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+    Config.set('graphics', 'width', 960)
+    Config.set('graphics', 'height', 565)
+
     SPDown().run()
